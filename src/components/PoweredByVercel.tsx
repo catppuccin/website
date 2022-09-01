f802e597a60b92e06ebfb5dfd2da26a96aa31685
winston <hey@winston.sh>
2022-09-01 12:05:53 +0200

    feat: add `PoweredByVercel.tsx`

---
+++ src/components/PoweredByVercel.tsx
@@ -0,0 +1,9 @@
+import VercelFooter from "../../powered-by-vercel.svg"
+
+export default function PoweredByVercel() {
+  return (
+    <div className="p-2">
+      <VercelFooter className="h-12" />
+    </div>
+  )
+}
