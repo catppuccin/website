59bcfac6b1dc8df2654c3b88c483813199868a8e
winston <hey@winston.sh>
2022-09-01 11:57:07 +0200

    feat: use `octokit.paginate` for Pagination

--- src/lib/getPorts.ts
+++ src/lib/getPorts.ts
@@ -1,31 +1,21 @@
-import { Octokit } from "@octokit/rest";
+import {Octokit} from "@octokit/rest";
 
 const octokit = new Octokit();
 
 export default async function GetPorts() {
-  const pageOne = octokit.rest.repos
-    .listForOrg({
-      org: "catppuccin",
-      type: "public",
-      per_page: 100,
-      page: 1,
-    })
-    .then(({ data }) => {
+  return octokit.paginate(octokit.rest.repos
+    .listForOrg, {
+    org: "catppuccin",
+    type: "public",
+  })
+    .then((data) => {
       return data
         .filter(
-          ({ name }) =>
-            ![
-              "wallpapers",
-              "toolbox",
-              "catppuccin",
-              ".github",
-              "website",
-              "discord-bot",
-              "template",
-              "palette",
-            ].includes(name)
+          ({topics = []}) => {
+            return !topics.includes("catppuccin-meta")
+          }
         )
-        .sort(({ stargazers_count: a }, { stargazers_count: b }) => {
+        .sort(({stargazers_count: a}, {stargazers_count: b}) => {
           if (b !== undefined && a !== undefined) {
             return b - a;
           } else {
@@ -34,46 +24,6 @@ export default async function GetPorts() {
         });
     })
     .catch((err) => {
-      console.log(err);
       return err;
     });
-
-  const pageTwo = octokit.rest.repos
-    .listForOrg({
-      org: "catppuccin",
-      type: "public",
-      per_page: 100,
-      page: 2,
-    })
-    .then(({ data }) => {
-      return data
-        .filter(
-          ({ name }) =>
-            ![
-              "wallpapers",
-              "toolbox",
-              "catppuccin",
-              ".github",
-              "website",
-              "discord-bot",
-              "template",
-              "palette",
-            ].includes(name)
-        )
-        .sort(({ stargazers_count: a }, { stargazers_count: b }) => {
-          if (b !== undefined && a !== undefined) {
-            return b - a;
-          } else {
-            return 0;
-          }
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-      return err;
-    });
-
-  return Promise.all([pageOne, pageTwo]).then(([pageOne, pageTwo]) => {
-    return [...pageOne, ...pageTwo];
-  });
 }
