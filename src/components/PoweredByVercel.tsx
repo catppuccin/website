8f454f09d4a6513f533932ef5dd67097cca50ed9
winston <hey@winston.sh>
2022-09-01 21:26:17 +0200

    fix: use campaign link in Vercel Footer

--- src/components/PoweredByVercel.tsx
+++ src/components/PoweredByVercel.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import VercelFooter from "../../powered-by-vercel.svg";
 
 export default function PoweredByVercel() {
   return (
     <div className="p-2">
-      <VercelFooter className="h-12" />
+      <Link href="https://vercel.com/?utm_source=catppuccin&utm_campaign=oss">
+        <a>
+          <VercelFooter className="h-12" />
+        </a>
+      </Link>
     </div>
   );
 }
