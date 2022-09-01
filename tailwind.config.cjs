44ce3dd9e369cab49087acee94b5b08c00887d75
winston <hey@winston.sh>
2022-09-01 12:05:20 +0200

    feat: add `markdownToHtml.ts`, @tailwind/typography, set up fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,31 @@ module.exports = {
       fontFamily: {
         "work-sans": ["Work Sans", "sans-serif"],
         "epilogue": ["Epilogue", "sans-serif"],
-      }
+        "inter": ["Inter", "sans-serif"],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.text'),
+            a: {
+              color: theme('colors.sky'),
+              '&:hover': {
+                color: theme('colors.blue')
+              },
+            },
+            'h1, h2, h3, h4, h5, h6': {
+              color: theme('colors.text'),
+            },
+            '*': {
+              color: theme('colors.text'),
+            },
+            code: {
+              color: theme('colors.rosewater'),
+            }
+          },
+        },
+      }),
     },
   },
-  plugins: [require("@catppuccin/tailwindcss")],
+  plugins: [require("@catppuccin/tailwindcss"), require("@tailwindcss/typography")],
 };
