b264717970c67877167e009d2e5eb1a86d9ce915
winston <hey@winston.sh>
2022-09-03 12:57:37 +0200

    feat: add `CopyToClipboardBtn` component

---
+++ src/components/CopyToClipboardBtn.tsx
@@ -0,0 +1,15 @@
+import { FaClipboard } from "react-icons/fa";
+import { useCopyToClipboard } from "../hooks";
+
+export default function CopyToClipboardBtn({ text }: { text: string }) {
+  const [value, copy] = useCopyToClipboard(500);
+  return (
+    <button
+      onClick={() => copy(text)}
+      className="flex items-center gap-2 bg-mantle rounded-lg p-2 text-text hover:bg-mantle/30 duration-300"
+    >
+      <FaClipboard className={value ? "animate-bounce duration-500" : ""} />
+      <span className="sr-only">{value ? "Copied!" : "Copy"}</span>
+    </button>
+  );
+}
