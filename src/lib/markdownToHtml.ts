85c02ee53fc9ea8ba5fc2e5f08a7b43df4b38487
winston <hey@winston.sh>
2022-09-03 13:01:44 +0200

    feat: extend `markdownToHtml.ts` for better syntax highlighting

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,10 +1,12 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
-import rehypeSanitize from "rehype-sanitize";
+import rehypeSanitize, { defaultSchema } from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeHighlight from "rehype-highlight";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import vim from "highlight.js/lib/languages/vim";
 
 export default async function markdownToHtml(
   markdown: string,
@@ -13,9 +15,29 @@ export default async function markdownToHtml(
   const result = await unified()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: options.allowHtml })
-    .use(options.allowHtml ? () => {} : rehypeSanitize)
+    .use(options.allowHtml ? () => {} : rehypeSanitize, {
+      ...defaultSchema,
+      attributes: {
+        ...defaultSchema.attributes,
+        code: [
+          // @ts-ignore-next-line
+          ...(defaultSchema.attributes.code || []),
+          // List of all allowed languages:
+          ["className", "language-js", "language-css", "language-md"],
+        ],
+      },
+    })
+    .use(rehypeHighlight, {
+      languages: { vim },
+      aliases: {
+        bash: "sh",
+        markdown: "md",
+      },
+      ignoreMissing: true,
+      subset: false,
+    })
+    .use(rehypeAutolinkHeadings)
     .use(rehypeExternalLinks, { rel: ["nofollow"] })
-    .use(rehypeHighlight)
     .use(rehypeStringify, { allowDangerousHtml: options.allowHtml })
     .process(markdown);
   return result.toString();
