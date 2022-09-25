67e371c333917245dda6685486c2eedd8cb568da
Mathieu Lapierre <mathieu.lapierre@majisti.com>
2022-09-25 06:03:19 -0400

    feat(ui): Small ui tweaks

--- src/pages/ports/[slug].tsx
+++ src/pages/ports/[slug].tsx
@@ -16,11 +16,11 @@ export default function PortPage({
 }): ReactElement {
   return (
     <Layout>
-      <div className="max-w-3xl prose font-epilogue text-text mx-auto p-4 pt-8 rounded-md items-baseline">
+      <div className="max-w-3xl w-full prose font-epilogue text-text mx-auto p-4 pt-8 rounded-md items-baseline">
         <div className="flex justify-between">
           <h1>Catppuccin for {name}</h1>
           <Link href={`https://github.com/catppuccin/${name}`}>
-            <a aria-label="Viw on GitHub">
+            <a aria-label="View on GitHub">
               <SiGithub size={"1.7rem"} />
               <p className="sr-only">View on GitHub</p>
             </a>
