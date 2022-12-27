bd2a69e790ab03730e947481426b2bd6b8f0cb04
winston <hey@winston.sh>
2022-12-27 03:04:00 +0100

    fix: await cache.put

--- src/lib/getPorts.ts
+++ src/lib/getPorts.ts
@@ -1,58 +1,58 @@
-import {Octokit} from "@octokit/rest";
-import cache from "memory-cache"
+import { Octokit } from "@octokit/rest";
+import cache from "memory-cache";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN || "" });
 
 export default async function GetPorts() {
   if (cache.get("repolist") !== null) {
-    return cache.get("repolist")
+    return cache.get("repolist");
   } else {
     const data = octokit
-        .paginate(octokit.rest.repos.listForOrg, {
-          org: "catppuccin",
-          type: "public",
-        })
-        .then((data) => {
-          return data
-            .filter(({topics = [], visibility}) => {
-              const isMeta = topics.includes("catppuccin-meta")
-              const isPrivate = visibility === "private"
-              return !(isMeta && isPrivate)
-            })
-            .sort(({stargazers_count: a}, {stargazers_count: b}) => {
-              if (b !== undefined && a !== undefined) {
-                return b - a;
-              } else {
-                return 0;
-              }
-            })
-            .map(
-              ({
-                 name,
-                 description,
-                 html_url,
-                 stargazers_count,
-                 forks_count,
-                 topics,
-                 default_branch,
-               }) => {
-                return {
-                  name,
-                  description,
-                  html_url,
-                  stargazers_count,
-                  forks_count,
-                  topics,
-                  default_branch,
-                };
-              }
-            );
-        })
-        .catch((err) => {
-          console.log(err);
-          return err;
-        });
-    cache.put("repolist", data)
-    return data
+      .paginate(octokit.rest.repos.listForOrg, {
+        org: "catppuccin",
+        type: "public",
+      })
+      .then((data) => {
+        return data
+          .filter(({ topics = [], visibility }) => {
+            const isMeta = topics.includes("catppuccin-meta");
+            const isPrivate = visibility === "private";
+            return !(isMeta && isPrivate);
+          })
+          .sort(({ stargazers_count: a }, { stargazers_count: b }) => {
+            if (b !== undefined && a !== undefined) {
+              return b - a;
+            } else {
+              return 0;
+            }
+          })
+          .map(
+            ({
+              name,
+              description,
+              html_url,
+              stargazers_count,
+              forks_count,
+              topics,
+              default_branch,
+            }) => {
+              return {
+                name,
+                description,
+                html_url,
+                stargazers_count,
+                forks_count,
+                topics,
+                default_branch,
+              };
+            }
+          );
+      })
+      .catch((err) => {
+        console.log(err);
+        return err;
+      });
+    await cache.put("repolist", data);
+    return data;
   }
 }
