1afa4458eb09dc78544306fd4dc41e93a3a3d9db
Andreas <andreas@grafen.info>
2024-03-03 02:31:18 +0100

    chore:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config'
-import yaml from '@rollup/plugin-yaml'
+import { defineConfig } from "astro/config";
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
-vite: {
+  vite: {
     plugins: [yaml()],
   },
-})
+});
--- src/contents/CatppuccinLinks.ts
+++ src/contents/CatppuccinLinks.ts
@@ -1,11 +1,9 @@
 export const CatppuccinLinks = {
-
-  github:         'https://github.com/catppuccin',
-  opencollective: 'https://opencollective.com/catppuccin',
-  discord:        'https://discord.com/servers/catppuccin-907385605422448742',
-  mastodon:       'https://fosstodon.org/@catppuccin',
-  twitter:        'https://twitter.com/catppuccintheme',
-  twitch:         'https://twitch.com/catppuccin',
-  instagram:      'https://www.instagram.com/catppuccintheme'
-
-}
+  github: "https://github.com/catppuccin",
+  opencollective: "https://opencollective.com/catppuccin",
+  discord: "https://discord.com/servers/catppuccin-907385605422448742",
+  mastodon: "https://fosstodon.org/@catppuccin",
+  twitter: "https://twitter.com/catppuccintheme",
+  twitch: "https://twitch.com/catppuccin",
+  instagram: "https://www.instagram.com/catppuccintheme",
+};
