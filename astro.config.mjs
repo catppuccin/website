8c0e4401d63edf7b9872257ddd6c3478f77462e1
Scott Semtner <sjsemtner@gmail.com>
2024-03-24 20:45:50 -0700

    refactor: add `@astrojs/sitemap` integration (#37)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://catppuccin.com",
   vite: {
-    plugins: [yaml()],
+    plugins: [yaml()]
   },
+  integrations: [sitemap()]
 });
