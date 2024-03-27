bed18ad99285fb2cf904c4498015af9116b71cde
Andreas <andreas@grafen.info>
2024-03-27 12:34:21 +0100

    refactor(icons): parse actual icon files instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://catppuccin.com",
   vite: {
-    plugins: [yaml()],
+    plugins: [yaml()]
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()]
 });
