e266ffd37fc8d736a83f1d2c31d57d3ed29a2301
sgoudham <sgoudham@gmail.com>
2024-06-16 06:02:26 +0100

    feat: style search bar

--- src/components/PortGrid.svelte
+++ src/components/PortGrid.svelte
@@ -2,7 +2,6 @@
   import Fuse from "fuse.js";
   import type { Port, Userstyle } from "../lib/ports";
   import PortCard from "./cards/PortCard.svelte";
-  import { fly } from "svelte/transition";
 
   export let ports: Array<Port | Userstyle>;
   let portGrid: Array<Port | Userstyle> = ports;
@@ -26,15 +25,20 @@
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
       searchTerm ? (portGrid = fuse.search(searchTerm).map((key) => key.item)) : (portGrid = ports);
-    }, 125);
+    }, 200);
   }
 </script>
 
-<div id="search-input-cont">
+<div class="search-bar">
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"
+    ><path
+      d="M232.49,215.51,185,168a92.12,92.12,0,1,0-17,17l47.53,47.54a12,12,0,0,0,17-17ZM44,112a68,68,0,1,1,68,68A68.07,68.07,0,0,1,44,112Z"
+    ></path
+    ></svg>
   <input
     type="text"
     id="search-field"
-    placeholder="Enter a port name or category..."
+    placeholder="Search port name or category..."
     autocomplete="off"
     bind:value={searchTerm}
     on:input={handleInput} />
@@ -61,6 +65,38 @@
 
 <style lang="scss">
   @use "../styles/utils";
+
+  .search-bar {
+    display: flex;
+    gap: var(--space-xs);
+    padding: var(--space-xs) var(--space-xs);
+    margin-block-end: var(--space-md);
+
+    border-color: var(--subtext0);
+    background-color: var(--mantle);
+    border-radius: var(--border-radius-normal);
+    color: var(--text);
+
+    input {
+      background-color: inherit;
+      border-color: inherit;
+      color: inherit;
+      border: none;
+      outline: none;
+      width: 100%;
+      font-size: large;
+    }
+  }
+
+  .search-bar:focus-within {
+    outline: solid;
+    outline-width: 2px;
+    outline-color: var(--blue);
+  }
+
+  input::placeholder {
+    color: var(--overlay2);
+  }
 
   .port-grid {
     @include utils.grid($column: 250px, $gap: var(--space-md));
