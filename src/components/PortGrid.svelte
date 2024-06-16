814bf552ef9316bf4eac6bddcf177d4b8da135c6
sgoudham <sgoudham@gmail.com>
2024-06-16 23:53:51 +0100

    feat: url search params & fix search bar on mobile

--- src/components/PortGrid.svelte
+++ src/components/PortGrid.svelte
@@ -4,7 +4,8 @@
   import PortCard from "./cards/PortCard.svelte";
 
   export let ports: Array<Port | Userstyle>;
-  let portGrid: Array<Port | Userstyle> = ports;
+  let portGrid: Array<Port | Userstyle> | undefined = undefined;
+  let debounceTimeout: NodeJS.Timeout;
 
   const fuse = new Fuse(ports, {
     keys: [
@@ -13,19 +14,24 @@
       { name: "name", weight: 0.4 },
     ],
     includeScore: false,
-    threshold: 0.4,
+    threshold: 0.3,
   });
 
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const query = urlParams.get("q");
+  const url = new URL(window.location.href);
+  let searchTerm = url.searchParams.get("q") ?? "";
+  if (url.searchParams.has("q")) {
+    handleInput();
+  } else {
+    portGrid = ports;
+  }
 
-  let debounceTimeout: NodeJS.Timeout;
-  let searchTerm: string = "";
   function handleInput() {
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(() => {
       searchTerm ? (portGrid = fuse.search(searchTerm).map((key) => key.item)) : (portGrid = ports);
-    }, 50);
+    }, 75);
+    url.searchParams.set("q", searchTerm);
+    window.history.pushState(null, "", url.toString());
   }
 </script>
 
@@ -46,9 +52,9 @@
 
 <div class="port-grid">
   {#key portGrid}
-    {#if portGrid.length === 0}
+    {#if searchTerm && portGrid && portGrid.length === 0}
       <slot name="no-results"></slot>
-    {:else if portGrid.length > 0}
+    {:else if portGrid && portGrid.length > 0}
       {#each portGrid as fields (fields.key)}
         <div style={`display: ${portGrid.includes(fields) ? "grid" : "none"}`}>
           <PortCard
@@ -86,6 +92,7 @@
       color: inherit;
       border: none;
       outline: none;
+      width: 100%;
       font-size: large;
     }
   }
