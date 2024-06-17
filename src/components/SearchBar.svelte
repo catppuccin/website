2bacd91a758a81427a5b9193b060216f91307bd5
sgoudham <sgoudham@gmail.com>
2024-06-17 13:34:25 +0100

    refactor: ensure input can be clicked anywhere & accented search icon

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
+  import MagnifyingGlass from "./svgs/MagnifyingGlass.svelte";
+
   export let searchTerm: string;
   export let handleInput: () => void;
 </script>
 
 <div class="search-bar">
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"
-    ><path
-      d="M232.49,215.51,185,168a92.12,92.12,0,1,0-17,17l47.53,47.54a12,12,0,0,0,17-17ZM44,112a68,68,0,1,1,68,68A68.07,68.07,0,0,1,44,112Z"
-    ></path
-    ></svg>
+  <MagnifyingGlass />
   <input
     type="text"
     id="search-field"
     aria-label="Search"
-    placeholder="Search port name or category..."
+    placeholder="Search port or category..."
     autocomplete="off"
     bind:value={searchTerm}
     on:input={handleInput} />
@@ -23,25 +21,24 @@
   @use "../styles/utils";
 
   .search-bar {
-    display: flex;
-    gap: var(--space-sm);
-    align-items: center;
-    padding: var(--space-xs) var(--space-xs);
-    margin-block-end: var(--space-md);
-
-    border-color: var(--subtext0);
     background-color: var(--mantle);
+    display: flex;
+    gap: var(--space-xs);
+    align-items: center;
+    margin-block-end: var(--space-md);
     border-radius: var(--border-radius-normal);
-    color: var(--text);
+    padding-inline: var(--space-xs);
 
     input {
+      padding: var(--space-sm) 0;
       background-color: inherit;
       border-color: inherit;
       color: inherit;
+      border-radius: inherit;
       border: none;
       outline: none;
       width: 100%;
-      font-size: large;
+      font-size: 2rem;
     }
   }
 
