1b859df3dbed9a067a9b08da7c02af240ceb027c
sgoudham <sgoudham@gmail.com>
2024-09-14 02:11:15 +0100

    fix: allow website deploys by allowing 404 GitHub profiles

--- src/components/lists/Avatar.svelte
+++ src/components/lists/Avatar.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
+  import maintainersWithoutAvatars from "../../lib/maintainersWithoutAvatars.json";
   export let username: string;
   export let size: number;
   export let wxh: number;
+  const isPlaceholder = maintainersWithoutAvatars.includes(username);
 </script>
 
-<img
-  alt="{username}'s Avatar"
-  src="/maintainers/{size}x{size}/{username}.webp"
-  width={wxh}
-  height={wxh}
-  loading="lazy" />
+{#if isPlaceholder}
+  <img
+    class="placeholder"
+    alt="Placeholder Avatar"
+    title="This user's GitHub profile was not found."
+    src="/maintainers/{size}x{size}/placeholder.webp"
+    width={wxh}
+    height={wxh}
+    loading="lazy" />
+{:else}
+  <img
+    alt="{username}'s Avatar"
+    src="/maintainers/{size}x{size}/{username}.webp"
+    width={wxh}
+    height={wxh}
+    loading="lazy" />
+{/if}
 
 <style lang="scss">
   img {
@@ -18,5 +31,9 @@
     background-color: var(--base);
     margin-right: -12px;
     border-radius: 50%;
+  }
+
+  .placeholder {
+    filter: grayscale(100%);
   }
 </style>
--- src/lib/getAvatars.ts
+++ src/lib/getAvatars.ts
@@ -1,23 +1,27 @@
+import { $ } from "bun";
+import { currentMaintainers, type Collaborator } from "./ports";
+
 const PUBLIC_DIR = "public/maintainers";
+const LIB_DIR = "src/lib";
 const IMAGE_QUALITY = 80;
 const REQUEST_SIZE = 256;
 const SIZES = [64, 256];
 
-import { $ } from "bun";
-import { currentMaintainers, type Collaborator } from "./ports";
+const maintainersWithoutAvatars: string[] = [];
 
 async function fetchAndProcessImage(maintainer: Collaborator) {
   const username = maintainer.url.split("/").pop();
 
   const response = await fetch(`${maintainer.url}.png?size=${REQUEST_SIZE}`);
   if (!response.ok) {
-    throw new Error(`Failed to fetch ${maintainer.url}: ${response.status} ${response.statusText}`);
+    console.warn(`Failed to fetch ${maintainer.url}: ${response.status} ${response.statusText}`);
+    maintainersWithoutAvatars.push(maintainer.username);
+    return;
   }
   const image = await response.blob();
 
   await Bun.write(`${PUBLIC_DIR}/${username}.png`, image);
 
-  await Promise.all(SIZES.map((size) => $`mkdir -p ${PUBLIC_DIR}/${size}x${size}`));
   await Promise.all(
     SIZES.map(
       (size) =>
@@ -27,7 +31,20 @@ async function fetchAndProcessImage(maintainer: Collaborator) {
 }
 
 try {
+  await Promise.all(SIZES.map((size) => $`mkdir -p ${PUBLIC_DIR}/${size}x${size}`));
   await Promise.all(currentMaintainers.map((maintainer) => fetchAndProcessImage(maintainer)));
+  await Bun.write(`${LIB_DIR}/maintainersWithoutAvatars.json`, JSON.stringify(maintainersWithoutAvatars));
+
+  // Only include placeholder assets in the public directory when we have failed
+  // to retrieve GitHub profile information for at least one maintainer.
+  if (maintainersWithoutAvatars.length > 0) {
+    console.log("Transferring placeholder images to public directory...");
+    await Promise.all(
+      SIZES.map(
+        (size) => $`cp ${LIB_DIR}/placeholder_${size}x${size}.webp ${PUBLIC_DIR}/${size}x${size}/placeholder.webp`,
+      ),
+    );
+  }
 } catch (e) {
   console.error("Processing failed: ", e);
   process.exit(1);
