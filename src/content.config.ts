6604f338dba7649a279bae21a7831a86359abbee
lemon <git@unseen.ninja>
2024-12-06 01:42:27 +0100

    chore(blog): stuff for hammy

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,22 +6,23 @@ export const blogCategoriesEnum = z.enum(CATEGORIES);
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
-  schema: z.object({
-    heroImage: z.string(),
-    heroAuthor: z.string(),
-    heroSource: z.string(),
-    title: z.string(),
-    summary: z.string(),
-    category: z.enum(CATEGORIES),
-    datePosted: z.coerce.date(),
-    dateUpdated: z.coerce.date().optional(),
-    draft: z.boolean().optional(),
-    author: z.object({
-      name: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      heroImage: image().refine((img) => img),
+      heroAuthor: z.string(),
+      heroSource: z.string(),
       title: z.string(),
-      github: z.string(),
+      summary: z.string(),
+      category: z.enum(CATEGORIES),
+      datePosted: z.coerce.date(),
+      dateUpdated: z.coerce.date().optional(),
+      draft: z.boolean().optional(),
+      author: z.object({
+        name: z.string(),
+        title: z.string(),
+        github: z.string(),
+      }),
     }),
-  }),
 });
 
 export const collections = { blog };
