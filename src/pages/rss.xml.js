397a5bb181b483eec266f9227d646992c93ad78c
sgoudham <sgoudham@gmail.com>
2024-12-11 22:53:46 +0000

    wip: add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,30 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+import { title } from "./blog/index.astro";
+import { description } from "./blog/index.astro";
+
+export async function GET(context) {
+  const blog = await getCollection("blog");
+  return rss({
+    title,
+    description,
+    site: context.site,
+    items: blog
+      .filter((post) => !post.data.draft)
+      .map((post) => ({
+        title: post.data.title,
+        description: post.data.summary,
+        pubDate: post.data.datePosted,
+        link: `/blog/${post.id}/`,
+        author: `${post.data.author.name}`,
+        customData: `<media:content
+          type="image/${post.data.hero.path.format}"
+          width="${post.data.hero.path.width}"
+          height="${post.data.hero.path.height}"
+          medium="image"
+          url="${context.site}${post.data.hero.path.src}" />
+      `,
+      })),
+  });
+}
