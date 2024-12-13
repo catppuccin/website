edf5e58b0fe5010e8d17bb65dce457073269550b
sgoudham <sgoudham@gmail.com>
2024-12-13 14:29:48 +0000

    chore: switch deps & increase padding

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
 
 import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
@@ -22,7 +22,7 @@ const remarkReadingTime = () => {
 export default defineConfig({
   markdown: {
     rehypePlugins: [
-      rehypeSlug,
+      rehypeHeadingIds,
       [
         rehypeAutolinkHeadings,
         {
