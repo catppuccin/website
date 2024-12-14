4d36dc695ebc80b14bec63874296886c2994abe7
sgoudham <sgoudham@gmail.com>
2024-12-14 08:43:33 +0000

    chore: add category

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const CATEGORIES = ["Announcement"] as const;
+const CATEGORIES = ["Announcement", "DevLog"] as const;
 export const blogCategoriesEnum = z.enum(CATEGORIES);
 
 const blog = defineCollection({
