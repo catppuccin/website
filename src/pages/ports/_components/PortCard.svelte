d410a3228b201974347d336f950054388e7b9ab4
WalkQuackBack <quacksareback@duck.com>
2025-04-20 14:18:59 -0400

    initial code commit

--- src/pages/ports/_components/PortCard.svelte
+++ src/pages/ports/_components/PortCard.svelte
@@ -5,9 +5,36 @@
   import PortMaintainers from "./PortMaintainers.svelte";
 
   let { port }: { port: PortWithIcons } = $props();
+
+  let isVisible = $state(false);
+  let element;
+
+  $effect(() => {
+    let observer = new IntersectionObserver(
+      (entries: HTMLElement[]) => {
+        for (let i = 0; i < entries.length; i++) {
+          const entry = entries[i];
+          isVisible = entry.isIntersecting;
+        }
+      },
+      {
+        threshold: 0.1,
+      },
+    );
+
+    if (element) {
+      observer.observe(element);
+    }
+
+    return () => {
+      if (observer) {
+        observer.disconnect();
+      }
+    };
+  });
 </script>
 
-<a href={port.repository.url} class="port-card">
+<a bind:this={element} href={port.repository.url} class="port-card" class:port-visible={isVisible}>
   <div class="port-header">
     <p class="port-name">{Array.isArray(port.name) ? port.name.join(", ") : port.name}</p>
     <Icon
@@ -43,35 +70,85 @@
     color: var(--subtext0);
     font-size: 1.6rem;
 
-    transition: all 300ms ease-in-out;
+    transition:
+      opacity 225ms cubic-bezier(0, 0, 0, 1),
+      translate 225ms cubic-bezier(0, 0, 0, 1),
+      scale 225ms cubic-bezier(0, 0, 0, 1);
 
-    &:hover {
-      transform: scale(102%);
+    will-change: opacity, scale;
 
-      background-position: top right;
+    &::before {
+      content: "";
+      position: absolute;
+      inset: 0;
+
+      border-radius: inherit;
+      background-color: var(--surface0);
+
+      transition: opacity 200ms cubic-bezier(0.2, 0, 0, 1);
+      will-change: opacity;
+      opacity: 0;
+
+      z-index: -1;
+    }
+
+    &:focus-visible {
+      scale: 100%;
 
       text-decoration: none;
 
-      @media (prefers-reduced-motion) {
-        transform: none;
+      &::before {
+        opacity: 1;
       }
     }
 
-    p {
+    @media (hover: hover) {
+      &:hover {
+        scale: 105%;
+
+        text-decoration: none;
+
+        &::before {
+          opacity: 1;
+        }
+      }
+    }
+
+    &:focus {
+      text-decoration: none;
+    }
+
+    &:active {
+      scale: 95%;
+
+      &::before {
+        opacity: 0.65;
+      }
+    }
+
+    & {
+      opacity: 1;
+      translate: 0 0;
+    }
+    &:not(.port-visible) {
+      opacity: 0;
+      scale: 0.9;
+      translate: 0 10%;
+    }
+  }
+  .port-header {
+    @include utils.flex($gap: var(--space-sm));
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    & > p {
       margin: 0;
       padding: 0;
     }
+  }
 
-    .port-header {
-      @include utils.flex($gap: var(--space-sm));
-      flex-wrap: nowrap;
-      justify-content: space-between;
-    }
-
-    .port-name {
-      color: var(--text);
-      font-size: 2rem;
-      font-weight: 600;
-    }
+  .port-name {
+    color: var(--text);
+    font-size: 2rem;
+    font-weight: 600;
   }
 </style>
